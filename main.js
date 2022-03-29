52de57ca7b39150a585c9822dc1353983d18aed3
Richard Lynch <lynchardrich@gmail.com>
2022-03-29 09:38:25 +0100

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+(async () => {
+    const jsonData = await fetch('/data.json').then(d => d.json())
+
+    function makeGraph(id, key) {
+        const { data, layout } = jsonData[key]
+        return Plotly.newPlot(id, data, layout, { responsive: true })
+    }
+
+    const graphs = {
+        'graph1': 'monthly',
+        'graph2': 'uniqueUsers',
+        'graph3': 'visitors',
+        'graph4': 'netloan'
+    }
+    Object.keys(graphs).map(id => makeGraph(id, graphs[id]))
+})()
